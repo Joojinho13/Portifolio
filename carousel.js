425e4ddb4716fe96298c5423aeb54f67522eca31
João Victor <joojvitor13@gmail.com>
2025-04-25 00:08:40 -0300

    Update 0.2
    Adicionado: Tags, Trailers nos carrosséis e melhores descrições para os Projetos e Sobre Mim

--- carousel.js
+++ carousel.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const carousels = document.querySelectorAll(".carousel");
-  
-    carousels.forEach((carousel) => {
-      const imagesContainer = carousel.querySelector(".carousel-images");
-      const images = imagesContainer.querySelectorAll("img");
-      const prevButton = carousel.querySelector(".prev");
-      const nextButton = carousel.querySelector(".next");
-      let currentIndex = 0;
-  
-      function updateCarousel() {
-        imagesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-      }
-  
-      nextButton.addEventListener("click", () => {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateCarousel();
-      });
-  
-      prevButton.addEventListener("click", () => {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        updateCarousel();
-      });
-  
+  const carousels = document.querySelectorAll(".carousel");
+
+  carousels.forEach((carousel) => {
+    const imagesContainer = carousel.querySelector(".carousel-images");
+
+    // Seleciona todos os elementos filhos como slides (img, div com vídeo, etc.)
+    const slides = Array.from(imagesContainer.children);
+
+    const prevButton = carousel.querySelector(".prev");
+    const nextButton = carousel.querySelector(".next");
+    let currentIndex = 0;
+
+    function updateCarousel() {
+      imagesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    nextButton.addEventListener("click", () => {
+      currentIndex = (currentIndex + 1) % slides.length;
       updateCarousel();
     });
+
+    prevButton.addEventListener("click", () => {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      updateCarousel();
+    });
+
+    updateCarousel();
   });
-  
+});
