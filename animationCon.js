6bda05d910985318bcec8aa6bac1a3ce7e5a7ea3
João Victor <joojvitor13@gmail.com>
2025-05-01 00:19:38 -0300

    Update 0.5
    Animações foram adicionadas na aba de navegação e adição de fade-in nos conteúdos das abas. Também foi adicionado um script para controlar as animações.
    
    Correção das imagens dos carrosséis, agora elas tem uma transição de conceito "infinito".
    
    Alteração no endereçamento das imagens dos carroséis.

---
+++ animationCon.js
@@ -0,0 +1,18 @@
+/* Fade-in */
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeElements = document.querySelectorAll('.fade-in');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  fadeElements.forEach(el => observer.observe(el));
+});
